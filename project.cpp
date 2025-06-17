5e091f5ed419f45b052e07757c09959eae5dd6b6
student skkumawat0047 <sk.kumawat0047@gmail.com>
2025-06-17 08:55:55 +0530

    customer class in feature1

--- project.cpp
+++ project.cpp
@@ -1,14 +1,15 @@
 #include<iostream>
 #include<string>
-#include<cctype>
+#include<iomanip>
 using namespace std;
-class Product{
-	int id;
-	public:
-	void input();
-//	bool namevalid();
-};
 
+class customer{
+	string name;
+	long long int mob_number;
+	public:
+		void cust_input();
+		void cust_display();
+};
 bool namevalid(const string& name){
 	if(name.empty()){
 		return false;
@@ -20,51 +21,46 @@ bool namevalid(const string& name){
 	}
 	return true;
 }
-void Product::input() {
-	int id;
-	string name;
-	float price;
-	float quantity;
-    cout << "Enter Product ID: ";
-    cin >> id;
-    if(cin.fail() || id<=0){
-    	cout<<"invelid input, plz enter in degits! "<<endl;
-    	cin.clear();
-    	cin.ignore(10000,'\n');
-    	input();
-    	return ;
-	}
-	cout<<"your id is: "<<id<<endl;
-	
-	while(true){
+int digit_mob(long long int mob_number){
+    int i=0;
+    while(mob_number>0){
+        mob_number=mob_number/10;
+        i++;
+    }
+    if(i==10) 
+        return 10;
+    else 
+       return 11;
+       
+}
+void customer :: cust_input() {
+    while(true){
 		cout<<"enter name: ";
 		getline(cin,name);
 		if(namevalid(name)) break;
 		    cout << "Invalid name. No digits/special characters allowed.\n";
 	}
-	
 	while(true){
-		cout<<"enter price: ";
-		cin>>price;
-		if(cin.fail() || price<=0){
+		cout<<"enter mob_number ";
+		cin>>mob_number;
+		if(cin.fail() || mob_number<=0 || (digit_mob(mob_number))==11) {
     	cout<<"invelid input, plz enter in degits! "<<endl;
     	cin.clear();
     	cin.ignore(10000,'\n');
     	}
     	else break;
 	}
-	
-	while (true) {
-        cout << "Enter Quantity: ";
-        cin >> quantity;
-        if (cin.fail() || quantity <= 0) {
-            cin.clear(); cin.ignore(1000, '\n');
-            cout << "Invalid quantity. Try again.\n";
-        } else break;
-    }
+    cout<<mob_number<<endl;
 }
-int main(){
-	Product p;
-	p.input();
+void customer::cust_display(){
+    cout<<"/------------customer-detials--------------/"<<endl;
+    cout<<setw(10)<<"name"<<setw(20)<<"mob_number"<<endl;
+    cout<<setw(10)<<name<<setw(20)<<mob_number<<endl;
+}
+int main()
+{
+	customer c;
+	c.cust_input();
+	c.cust_display();
 	return 0;
 }
